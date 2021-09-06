b5d97df07c6aea51f1ebf8797c2ed46d1a793264
Mathijsvanginneken <mathijsvginneken@gmail.com>
2021-09-06 14:57:52 +0000

    Deleted instructors file, working now

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,14 +9,3 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
-const express = require('express');
-const router = express.Router();
-
-/* GET home page */
-
-// Code the  get('/') route here rendering index
-router.all('/', (req, res) => {
-  res.render('index', {title: "Home Page TEST"})
-});
-
-module.exports = router;
